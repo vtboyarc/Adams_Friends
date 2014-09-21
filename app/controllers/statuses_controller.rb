41257c31df70854990b0819853fad6c06227a9d3
Adam Carter <adamrcarter@gmail.com>
2014-09-21 11:17:52 -0500

    made some changes to controller

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -43,8 +43,8 @@ class StatusesController < ApplicationController
   # PATCH/PUT /statuses/1.json
   def update
     @status = current_user.statuses.find(params[:id])
-    if (params[:status])
-      params[:status].delete(:user_id) if params[:status].has_key?(:user_id)
+    if params[:status] && params[:status].has_key?(:user_id)
+      params[:status].delete(:user_id)
     end
 
     respond_to do |format|
