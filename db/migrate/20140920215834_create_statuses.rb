1c84511d3074f3bd2c11467f88fcb8488a5cef80
Adam Carter <adamrcarter@gmail.com>
2014-09-20 23:04:27 -0500

    db changes

--- db/migrate/20140920215834_create_statuses.rb
+++ db/migrate/20140920215834_create_statuses.rb
@@ -1,10 +1,3 @@
 class CreateStatuses < ActiveRecord::Migration
-  def change
-    create_table :statuses do |t|
-      t.string :name
-      t.text :content
-
-      t.timestamps
-    end
-  end
+  
 end
